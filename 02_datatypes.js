bac7d854f09b16df4b14769facdd9cfcee91dce4
kunalpuri123 <112803004+kunalpuri123@users.noreply.github.com>
2024-05-13 15:21:53 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+// alert(3+3) //we are using nodejs,not browser
+console.log(3
+    +
+    3) // code readiblity should be high
+
+    //DATATYPES
+    //PRIMITIVE DATATYPE
+    //number 
+    //bigint
+    //string=> " "
+    //boolean => true/false
+    //null =>standalone value
+    //undefined => value not assigned
+    //symbol =>uniqueness
+
+    //object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
