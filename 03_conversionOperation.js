4b19c5ff4e106a4b331e462e879003efbfd2421d
kunalpuri123 <112803004+kunalpuri123@users.noreply.github.com>
2024-05-13 15:37:03 +0000

    conversionOperation

---
+++ 03_conversionOperation.js
@@ -0,0 +1,15 @@
+const score ="33"
+const value="33avc"
+
+console.log(typeof score)
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+let secondInNumber =Number(value)
+console.log(typeof secondInNumber);
+console.log(secondInNumber);//Beaware gives NaN(NOT A NUMBER)
+// IF NULL RETURN 0
+// IF undefined RETURNS nan
+
+//1=>true 0=>false " "=>false "ghvg"=>true
