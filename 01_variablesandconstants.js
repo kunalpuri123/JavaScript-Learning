fd7d1a2dfdb5695571349d92f59fee7b0cd87373
kunalpuri123 <112803004+kunalpuri123@users.noreply.github.com>
2024-05-13 15:05:19 +0000

    variableandconstant

---
+++ 01_variablesandconstants.js
@@ -0,0 +1,15 @@
+const addNumber = 12345;
+let accEmail = "kunal123@gmail.com"
+var accPassWord = "12345"
+accCity="Jaipur"
+let accountState; //undefined value
+
+// addNumber =2 //Not allowed
+accEmail="add@gmail.com"
+accPassWord="2555"
+// console.log(accountId);
+console.table([accEmail,accPassWord,addNumber])
+
+/*
+dont use var because of issue in scope
+*/
